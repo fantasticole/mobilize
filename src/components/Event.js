5ca00196d3d30dec717c1e7439283bca352c5bf6
Cole Murphy <cole@spell.run>
2018-10-05 18:13:44 -0400

    clean up list view

--- src/components/Event.js
+++ src/components/Event.js
@@ -24,24 +24,26 @@ class Event extends Component {
 
     if (expanded) {
       return (
-        <div className="eventListItem" key={event.id} onClick={this.toggleDetails}>
-          {event.featured_image_url && (<img src={event.featured_image_url} alt={event.summary} />)}
-          <a href={event.browser_url}>
+        <div className="eventListItem expanded" key={event.id} onClick={this.toggleDetails}>
+          <div className="imgCont">
+            {event.featured_image_url && <img src={event.featured_image_url} alt={event.summary} />}
+          </div>
+          <div className="eventInfo">
             <h3>{event.title}</h3>
-            <p>({event.timezone})</p>
-          </a>
-          <p className="eventDesc">{event.description}</p>
-          <div className="eventDetail">
-            <p className="label">Type: </p>
-            <p className="detail">{event.event_type}</p>
-          </div>
-          <div className="eventDetail">
-            <p className="label">Created: </p>
-            <p className="detail">{event.created_date}</p>
-          </div>
-          <div className="eventDetail">
-            <p className="label">Updated: </p>
-            <p className="detail">{event.modified_date}</p>
+            <a href={event.browser_url} target="_blank" rel="noopener noreferrer">event site</a>
+            <p className="eventDesc">{event.description}</p>
+            <div className="eventDetail">
+              <p className="label">Type: </p>
+              <p className="detail">{event.event_type}</p>
+            </div>
+            <div className="eventDetail">
+              <p className="label">Created: </p>
+              <p className="detail">{event.created_date}</p>
+            </div>
+            <div className="eventDetail">
+              <p className="label">Updated: </p>
+              <p className="detail">{event.modified_date}</p>
+            </div>
           </div>
         </div>
       );
@@ -49,12 +51,14 @@ class Event extends Component {
 
     return (
       <div className="eventListItem" key={event.id} onClick={this.toggleDetails}>
-        {event.featured_image_url && (<img src={event.featured_image_url} alt={event.summary} />)}
-        <a href={event.browser_url}>
+        <div className="imgCont">
+          {event.featured_image_url && <img src={event.featured_image_url} alt={event.summary} />}
+        </div>
+        <div className="eventInfo">
           <h3>{event.title}</h3>
-          <p>({event.timezone})</p>
-        </a>
-        <p className="eventSum">{event.summary}</p>
+          <a href={event.browser_url} target="_blank" rel="noopener noreferrer">event site</a>
+          <p className="eventSum">{event.summary}</p>
+        </div>
       </div>
     );
   }
--- src/components/ListView.js
+++ src/components/ListView.js
@@ -5,19 +5,12 @@ import Event from "./Event";
 
 import { eventPropType } from "../utils/propTypes";
 
-  // contact: PropTypes.????,
-  // high_priority: PropTypes.????,
-  // location: locationPropType,
-  // sponsor: sponsorPropType,
-  // timeslots: PropTypes.arrayOf(timeslotPropType),
-
 class ListView extends Component {
   render() {
     const { events, error } = this.props;
 
     return (
       <div className="events">
-        <h1>events:</h1>
         {error && <p className="error">{error.message}</p>}
         <div className="eventList">
           {events.map(event => (<Event event={event} key={event.id}/>))}
