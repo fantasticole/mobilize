8fd5fc155edfc8b4b60ac56da31ed11f543e16f9
Cole Murphy <cole@spell.run>
2018-10-05 17:55:25 -0400

    Add more information to the map

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -13,15 +13,8 @@ class MapView extends Component {
   }
 
   componentDidMount() {
-    const { mapboxgl } = window;
-
-    if (mapboxgl) {
-      mapboxgl.accessToken = "pk.eyJ1IjoiZmFudGFzdGljb2xlIiwiYSI6ImNqbXdncWpvdzAwcHIzcmxscGx6cnF0OWQifQ.uLqi6l2hvGms7WHbI6Hm7Q";
-      this.map = new mapboxgl.Map({
-        container: this.mapContainer,
-        style: "mapbox://styles/mapbox/streets-v9"
-      });
-      this.setMap();
+    if (window.mapboxgl) {
+      this.createMap();
     } else {
       this.setState({
         error: true,
@@ -39,26 +32,73 @@ class MapView extends Component {
     }
   }
 
+  createMap = () => {
+    const { mapboxgl } = window;
+
+    // set Mapbox access token
+    mapboxgl.accessToken = "pk.eyJ1IjoiZmFudGFzdGljb2xlIiwiYSI6ImNqbXdncWpvdzAwcHIzcmxscGx6cnF0OWQifQ.uLqi6l2hvGms7WHbI6Hm7Q";
+    // create the map, setting it in the "#map" div
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v9",
+    });
+    // show nav in upper left-hand corner
+    const nav = new mapboxgl.NavigationControl({
+      showCompass: true,
+      showZoom: true,
+    });
+    this.map.addControl(nav, "top-left");
+    // set map bounds and markers
+    this.setMap();
+  }
+
   setMap = () => {
     const { mapboxgl } = window;
 
     if (this.props.events.length) {
       // filter events for those with locations set
       const locs = this.props.events.filter(event => (event.location && event.location.location));
-      // get the coordinates of each location
-      const coordinates = locs.map(e => ([
-        e.location.location.longitude,
-        e.location.location.latitude,
-      ]));
-
-      // send those coordinates to the map
-      new mapboxgl.LngLatBounds(coordinates);
-      // var v2 = new mapboxgl.LngLatBounds([-73.9580807, 40.6663495])
+      const first = locs[0].location.location;
+      // get the outermost coordinates
+      let minLat = first.latitude;
+      let minLon = first.longitude;
+      let maxLat = first.latitude;
+      let maxLon = first.longitude;
       locs.forEach(l => {
-        const { location } = l.location;
-        const coor = [location.longitude, location.latitude];
+        if (l.location.location.longitude > maxLon) {
+          maxLon = l.location.location.longitude;
+        }
+        if (l.location.location.longitude < minLon) {
+          minLon = l.location.location.longitude;
+        }
+        if (l.location.location.latitude > maxLat) {
+          maxLat = l.location.location.latitude;
+        }
+        if (l.location.location.latitude < minLat) {
+          minLat = l.location.location.latitude;
+        }
+      });
+      // create the map's bounds
+      const mapBounds = new mapboxgl.LngLatBounds([
+        [minLon, minLat],
+        [maxLon, maxLat],
+      ]);
+      // apply those bounds to the map
+      this.map.fitBounds(mapBounds);
+
+      // create a marker with a popup for each event
+      locs.forEach(e => {
+        const { location } = e.location;
+        const popup = new mapboxgl.Popup({ offset: 25 })
+          .setHTML(`
+            <h3>${e.title}</h3>
+            <a href="${e.browser_url}">event site</a>
+            <p>${e.summary}</p>
+          `);
+
         new mapboxgl.Marker()
-          .setLngLat(coor)
+          .setLngLat([location.longitude, location.latitude])
+          .setPopup(popup)
           .addTo(this.map);
       })
     }
