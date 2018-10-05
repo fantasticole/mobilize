d21e6b93b9093cf9906fe914b3ee7d1259fc4a7d
Cole Murphy <cole@spell.run>
2018-10-05 15:37:18 -0400

    Begin validating prop types

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import { eventPropType } from "../utils/propTypes";
 
 class ListView extends Component {
   render() {
+    console.log(this.props)
     return (
       <h1>list</h1>
     );
   }
 }
+
+ListView.propTypes = {
+  error: PropTypes.shape({
+    message: PropTypes.string,
+  }),
+  events: PropTypes.arrayOf(eventPropType),
+};
+
+ListView.defaultProps = {
+  error: null,
+  events: false,
+};
 
 export default ListView;
---
+++ src/utils/propTypes.js
@@ -0,0 +1,56 @@
+import PropTypes from "prop-types";
+
+export const sponsorPropType = {
+  candidate_name: PropTypes.string,
+  created_date: PropTypes.num,
+  district: PropTypes.string,
+  event_feed_url: PropTypes.string,
+  id: PropTypes.num,
+  is_coordinated: PropTypes.bool,
+  is_independent: PropTypes.bool,
+  is_primary_campaign: PropTypes.bool,
+  modified_date: PropTypes.num,
+  name: PropTypes.string,
+  race_type: PropTypes.string,
+  slug: PropTypes.string,
+  state: PropTypes.string,
+};
+
+export const locationPropType = {
+  address_lines: PropTypes.arrayOf(PropTypes.string),
+  // congressional_district: null
+  locality: PropTypes.string,
+  location: PropTypes.shape({
+    latitude: PropTypes.num,
+    longitude: PropTypes.num,
+  }),
+  postal_code: PropTypes.string,
+  region: PropTypes.string,
+  // state_leg_district: null
+  // state_senate_district: null
+  venue: PropTypes.string,
+};
+
+export const timeslotPropType = {
+  end_date: PropTypes.num,
+  id: PropTypes.num,
+  start_date: PropTypes.num,
+};
+
+export const eventPropType = {
+  browser_url: PropTypes.string,
+  // contact: PropTypes.string,
+  created_date: PropTypes.num,
+  description: PropTypes.string,
+  event_type: PropTypes.string,
+  featured_image_url: PropTypes.string,
+  // high_priority: PropTypes.string,
+  id: PropTypes.num,
+  location: locationPropType,
+  modified_date: PropTypes.num,
+  sponsor: sponsorPropType,
+  summary: PropTypes.string,
+  timeslots: PropTypes.arrayOf(timeslotPropType),
+  timezone: PropTypes.string,
+  title: PropTypes.string,
+}
