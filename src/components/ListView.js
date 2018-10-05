5335114a7bcba3fa48605971e89554dbb6af0614
Cole Murphy <cole@spell.run>
2018-10-05 14:59:54 -0400

    Replace single quotes with doubles, organize files  and begin creating components

---
+++ src/components/ListView.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ListView extends Component {
+  render() {
+    return (
+      <h1>list</h1>
+    );
+  }
+}
+
+export default ListView;
---
+++ src/components/MapView.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class MapView extends Component {
+  render() {
+    return (
+      <h1>map</h1>
+    );
+  }
+}
+
+export default MapView;
